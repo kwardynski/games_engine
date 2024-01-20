631c9276651a4f8548e70b9f2a935378539cfdc4
kwardynski <kacper.wardynski@gmail.com>
2024-01-20 07:49:39 -0700

    locals without parents, spec/2 and inspect/1

--- lib/games_engine/validations/grid_validations.ex
+++ lib/games_engine/validations/grid_validations.ex
@@ -6,10 +6,8 @@ defmodule GamesEngine.Validations.GridValidations do
   @doc """
   Ensures a linear index is within the bounds of the supplied grid
   """
-  @spec(
-    ind_within_bounds(non_neg_integer(), {non_neg_integer(), non_neg_integer()}) :: :ok,
-    {:error, String.t()}
-  )
+  @spec ind_within_bounds(non_neg_integer(), {non_neg_integer(), non_neg_integer()}) :: :ok,
+        {:error, String.t()}
   def ind_within_bounds(ind, {rows, cols}) do
     if ind >= rows * cols,
       do: {:error, "#{ind} exceeds the bounds of a #{rows}x#{cols} board"},
--- lib/games_engine/validations/numeric_validations.ex
+++ lib/games_engine/validations/numeric_validations.ex
@@ -6,9 +6,9 @@ defmodule GamesEngine.Validations.NumericValidations do
   @doc """
   Ensures the input is a non-negative integer
   """
-  @spec(non_neg_integer(term()) :: :ok, {:error, String.t()})
+  @spec non_neg_integer(term()) :: :ok, {:error, String.t()}
   def non_neg_integer(input) when is_integer(input) and input >= 0, do: :ok
 
   def non_neg_integer(input),
-    do: {:error, "Expected non-negative integer, received #{inspect(input)}"}
+    do: {:error, "Expected non-negative integer, received #{inspect input}"}
 end
