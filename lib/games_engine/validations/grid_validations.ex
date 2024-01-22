4c4e4ad07a0b9ff4da24f7adb1b0fa6a32e9bda0
Kacper Wardynski <kacper.wardynski@gmail.com>
2024-01-21 17:35:15 -0700

    feat(GRID-2): pass grid to grid validations (#19)

--- lib/games_engine/validations/grid_validations.ex
+++ lib/games_engine/validations/grid_validations.ex
@@ -3,9 +3,16 @@ defmodule GamesEngine.Validations.GridValidations do
   Functions with perform validations of a game grid
   """
 
+  alias GamesEngine.Grid
+
   @doc """
   Ensures a linear index is within the bounds of the supplied grid
   """
+  @spec ind_within_bounds(non_neg_integer(), Grid.t()) :: :ok, {:error, String.t()}
+  def ind_within_bounds(ind, %Grid{} = grid) do
+    ind_within_bounds(ind, grid.rows, grid.cols)
+  end
+
   @spec ind_within_bounds(non_neg_integer(), non_neg_integer(), non_neg_integer()) :: :ok,
         {:error, String.t()}
   def ind_within_bounds(ind, rows, cols) do
@@ -17,6 +24,12 @@ defmodule GamesEngine.Validations.GridValidations do
   @doc """
   Ensures a row/col subscript is within the bounds of the supplied grid
   """
+  @spec sub_within_bounds({non_neg_integer(), non_neg_integer()}, Grid.t()) ::
+          :ok | {:error, String.t()}
+  def sub_within_bounds({row, col}, %Grid{} = grid) do
+    sub_within_bounds({row, col}, grid.rows, grid.cols)
+  end
+
   @spec sub_within_bounds(
           {non_neg_integer(), non_neg_integer()},
           non_neg_integer(),
--- test/games_engine/validations/grid_validations_test.exs
+++ test/games_engine/validations/grid_validations_test.exs
@@ -1,7 +1,25 @@
 defmodule GamesEngine.Validations.GridValidationsTest do
   use ExUnit.Case, async: true
 
+  alias GamesEngine.Grid
   alias GamesEngine.Validations.GridValidations
+
+  describe "ind_within_bounds/2" do
+    setup do
+      [
+        grid: %Grid{rows: 3, cols: 3}
+      ]
+    end
+
+    test "returns error tuple if ind exceeds bounds of grid", %{grid: grid} do
+      assert {:error, "12 exceeds the bounds of a 3x3 board"} =
+               GridValidations.ind_within_bounds(12, grid)
+    end
+
+    test "returns :ok if ind within bounds of grid", %{grid: grid} do
+      assert :ok == GridValidations.ind_within_bounds(8, grid)
+    end
+  end
 
   describe "ind_within_bounds/3" do
     test "returns error tuple if ind exceeds bounds of grid" do
@@ -11,6 +29,26 @@ defmodule GamesEngine.Validations.GridValidationsTest do
 
     test "returns :ok if ind within bounds of grid" do
       assert :ok == GridValidations.ind_within_bounds(8, 3, 3)
+    end
+  end
+
+  describe "sub_within_bounds/2" do
+    setup do
+      [
+        grid: %Grid{rows: 3, cols: 3}
+      ]
+    end
+
+    test "returns error tuple if sub exceeds bounds of grid", %{grid: grid} do
+      assert {:error, "(3, 1) exceeds the bounds of a 3x3 board"} =
+               GridValidations.sub_within_bounds({3, 1}, grid)
+
+      assert {:error, "(1, 3) exceeds the bounds of a 3x3 board"} =
+               GridValidations.sub_within_bounds({1, 3}, grid)
+    end
+
+    test "returns :ok if sub within bounds of grid", %{grid: grid} do
+      assert :ok == GridValidations.sub_within_bounds({1, 1}, grid)
     end
   end
 
