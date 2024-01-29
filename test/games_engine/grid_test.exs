db0b71a33d84fa731427068d05e9e11139b7084b
kwardynski <kacper.wardynski@gmail.com>
2024-01-28 18:35:17 -0700

    update nested tile attributes from grid module

--- test/games_engine/grid_test.exs
+++ test/games_engine/grid_test.exs
@@ -1,6 +1,8 @@
 defmodule GamesEngine.GridTest do
   use ExUnit.Case, async: true
 
+  import GamesEngine.GridFixtures
+
   alias GamesEngine.Grid
 
   describe "new/2" do
@@ -43,6 +45,84 @@ defmodule GamesEngine.GridTest do
       Enum.each(tiles, fn {_, %{attributes: tile_attributes}} ->
         assert tile_attributes == default_attributes
       end)
+    end
+  end
+
+  describe "replace_tile_attributes/3" do
+    test "returns error tuple if new attributes arg is not a map" do
+      grid = grid_fixture(3, 3)
+
+      assert {:error, "expected map for :attributes"} =
+               Grid.replace_tile_attributes(grid, 3, "invalid_input")
+    end
+
+    test "successfully replaces the attributes of a tile" do
+      grid = grid_fixture(3, 3)
+      attributes = %{key: "value"}
+
+      assert %Grid{
+               tiles: %{
+                 3 => %{attributes: replaced_tile_attributes}
+               }
+             } = Grid.replace_tile_attributes(grid, 3, attributes)
+
+      assert replaced_tile_attributes == attributes
+    end
+  end
+
+  describe "update_tile_attribute/4" do
+    test "no change to tile attributes if a new attribute is supplied" do
+      attributes = %{key: "value"}
+      grid = grid_fixture(3, 3, attributes)
+
+      assert %Grid{
+               tiles: %{
+                 3 => %{attributes: tile_attributes}
+               }
+             } = Grid.update_tile_attribute(grid, 3, :new_key, "new_value")
+
+      assert tile_attributes == attributes
+    end
+
+    test "successfully updates an existing tile attribute" do
+      attributes = %{key: "value"}
+      grid = grid_fixture(3, 3, attributes)
+
+      assert %Grid{
+               tiles: %{
+                 3 => %{attributes: updated_tile_attributes}
+               }
+             } = Grid.update_tile_attribute(grid, 3, :key, "new_value")
+
+      assert updated_tile_attributes == %{key: "new_value"}
+    end
+  end
+
+  describe "add_tile_attribute/4" do
+    test "no change to tile attributes if a existing attribute is supplied" do
+      attributes = %{key: "value"}
+      grid = grid_fixture(3, 3, attributes)
+
+      assert %Grid{
+               tiles: %{
+                 3 => %{attributes: tile_attributes}
+               }
+             } = Grid.add_tile_attribute(grid, 3, :key, "new_value")
+
+      assert tile_attributes == attributes
+    end
+
+    test "successfully adds a new attribute to the tile" do
+      attributes = %{key: "value"}
+      grid = grid_fixture(3, 3, attributes)
+
+      assert %Grid{
+               tiles: %{
+                 3 => %{attributes: updated_tile_attributes}
+               }
+             } = Grid.add_tile_attribute(grid, 3, :new_key, "new_value")
+
+      assert updated_tile_attributes == %{key: "value", new_key: "new_value"}
     end
   end
 end
