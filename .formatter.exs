44e3bfaf13b9cae07b323f6710eb5f4f28ac72bb
Kacper Wardynski <kacper.wardynski@gmail.com>
2024-09-09 21:38:39 -0600

    bump line length to 115

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
   import_deps: [:stream_data],
   locals_without_parens: [inspect: 1, spec: 2],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 115
 ]
