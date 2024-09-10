7c125ff7c37d8785beb48ecb3e5427335aab4c07
Kacper Wardynski <kacper.wardynski@gmail.com>
2024-09-09 23:03:48 -0600

    bounce function and tests

--- lib/games_engine/physics.ex
+++ lib/games_engine/physics.ex
@@ -5,6 +5,8 @@ defmodule GamesEngine.Physics do
   alias GamesEngine.Grid.Coordinate
   alias GamesEngine.Physics.Velocity
 
+  @type bounce_type() :: :horizontal | :vertical
+
   @doc """
   Calculate's a `%Coordinate{}`'s new position based on a `%Velocity{}`
   """
@@ -14,4 +16,15 @@ defmodule GamesEngine.Physics do
 
     %{coordinate | row: new_row, col: new_col}
   end
+
+  @doc """
+  Returns a `%Velocity{}` with updated speed components after a horizontal
+  or vertical bounce
+
+  A :horizontal bounce reflects the `x` component, and a :vertical bounce
+  reflects the `y` component
+  """
+  @spec bounce(Velocity.t(), bounce_type()) :: Velocity.t()
+  def bounce(%Velocity{} = velocity, :horizontal), do: %{velocity | x: -velocity.x}
+  def bounce(%Velocity{} = velocity, :vertical), do: %{velocity | y: -velocity.y}
 end
