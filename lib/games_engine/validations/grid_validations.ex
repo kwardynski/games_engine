627635a7ace5ec233c99b81219a79604017a46b5
Kacper Wardynski <kacper.wardynski@gmail.com>
2024-09-17 23:43:49 -0600

    new outside bounds validation

--- lib/games_engine/validations/grid_validations.ex
+++ lib/games_engine/validations/grid_validations.ex
@@ -4,6 +4,14 @@ defmodule GamesEngine.Validations.GridValidations do
   """
 
   alias GamesEngine.Grid
+  alias GamesEngine.Grid.Point
+
+  @type point_within_bounds_errors :: %{
+          top: boolean(),
+          bottom: boolean(),
+          left: boolean(),
+          right: boolean()
+        }
 
   @doc """
   Ensures a linear index is within the bounds of the supplied grid
@@ -40,4 +48,55 @@ defmodule GamesEngine.Validations.GridValidations do
       do: :ok,
       else: {:error, "(#{row}, #{col}) exceeds the bounds of a #{rows}x#{cols} board"}
   end
+
+  @doc """
+  Determines whether a `%Point{}` is within the bounds of a `%Grid{}.
+  If the `%Point{}`'s coordinates are outside the `%Grid{}`, will return an error tuple
+  along with the direction(s) which the `%Point{}` is outside the `%Grid{}`
+
+  ## Examples
+
+      iex> grid = %GamesEngine.Grid{rows: 10, cols: 10}
+      iex> point = %GamesEngine.Grid.Point{x: 3, y: 5.4}
+      iex> GamesEngine.Validations.GridValidations.point_within_bounds(point, grid)
+      :ok
+
+      iex> grid = %GamesEngine.Grid{rows: 10, cols: 10}
+      iex> point = %GamesEngine.Grid.Point{x: 12, y: -7.3}
+      iex> GamesEngine.Validations.GridValidations.point_within_bounds(point, grid)
+      {:error, %{top: true, bottom: false, right: true, left: false}}
+
+  """
+  @spec point_within_bounds(Point.t(), Grid.t()) :: :ok, {:error, point_within_bounds()}
+  def point_within_bounds(%Point{} = point, %Grid{} = grid) do
+    errors = %{
+      top: false,
+      bottom: false,
+      left: false,
+      right: false
+    }
+
+    errors =
+      errors
+      |> check_if_outside_top(point, grid)
+      |> check_if_outside_bottom(point, grid)
+      |> check_if_outside_left(point, grid)
+      |> check_if_outside_right(point, grid)
+
+    if Enum.any?(errors, fn {_dir, outside_bounds?} -> outside_bounds? end),
+      do: {:error, errors},
+      else: :ok
+  end
+
+  defp check_if_outside_top(errors, %Point{y: y}, _grid) when y < 0, do: %{errors | top: true}
+  defp check_if_outside_top(errors, _point, _grid), do: errors
+
+  defp check_if_outside_bottom(errors, %Point{y: y}, %Grid{rows: rows}) when y > rows, do: %{errors | bottom: true}
+  defp check_if_outside_bottom(errors, _point, _grid), do: errors
+
+  defp check_if_outside_left(errors, %Point{x: x}, _grid) when x < 0, do: %{errors | left: true}
+  defp check_if_outside_left(errors, _pont, _grid), do: errors
+
+  defp check_if_outside_right(errors, %Point{x: x}, %Grid{cols: cols}) when x > cols, do: %{errors | right: true}
+  defp check_if_outside_right(errors, _pont, _grid), do: errors
 end
--- test/games_engine/validations/grid_validations_test.exs
+++ test/games_engine/validations/grid_validations_test.exs
@@ -1,7 +1,10 @@
 defmodule GamesEngine.Validations.GridValidationsTest do
   use ExUnit.Case, async: true
 
+  doctest GamesEngine.Validations.GridValidations
+
   alias GamesEngine.Grid
+  alias GamesEngine.Grid.Point
   alias GamesEngine.Validations.GridValidations
 
   describe "ind_within_bounds/2" do
@@ -55,6 +58,79 @@ defmodule GamesEngine.Validations.GridValidationsTest do
 
     test "returns :ok if sub within bounds of grid" do
       assert :ok == GridValidations.sub_within_bounds({1, 1}, 2, 2)
+    end
+  end
+
+  describe "point_within_bounds/2" do
+    setup do
+      [
+        grid: %Grid{rows: 10, cols: 10}
+      ]
+    end
+
+    test "returns :ok if point within bounds of grid", %{grid: grid} do
+      point = %Point{x: 1, y: 3.5}
+      assert :ok == GridValidations.point_within_bounds(point, grid)
+    end
+
+    test "returns error tuple if point exceeds top bound of grid", %{grid: grid} do
+      point = %Point{x: 7, y: -1.1}
+      {:error, errors} = GridValidations.point_within_bounds(point, grid)
+
+      assert %{
+               top: true,
+               bottom: false,
+               left: false,
+               right: false
+             } = errors
+    end
+
+    test "returns error tuple if point exceeds bottom bound of grid", %{grid: grid} do
+      point = %Point{x: 7, y: 10.1}
+      {:error, errors} = GridValidations.point_within_bounds(point, grid)
+
+      assert %{
+               top: false,
+               bottom: true,
+               left: false,
+               right: false
+             } = errors
+    end
+
+    test "returns error tuple if point exceeds left bound of grid", %{grid: grid} do
+      point = %Point{x: -12, y: 7}
+      {:error, errors} = GridValidations.point_within_bounds(point, grid)
+
+      assert %{
+               top: false,
+               bottom: false,
+               left: true,
+               right: false
+             } = errors
+    end
+
+    test "returns error tuple if point exceeds right bound of grid", %{grid: grid} do
+      point = %Point{x: 12.1, y: 7.3}
+      {:error, errors} = GridValidations.point_within_bounds(point, grid)
+
+      assert %{
+               top: false,
+               bottom: false,
+               left: false,
+               right: true
+             } = errors
+    end
+
+    test "returns error tuple if point exceeds bounds of grid in multiple directions", %{grid: grid} do
+      point = %Point{x: -0.1, y: 11}
+      {:error, errors} = GridValidations.point_within_bounds(point, grid)
+
+      assert %{
+               top: false,
+               bottom: true,
+               left: true,
+               right: false
+             } = errors
     end
   end
 end
